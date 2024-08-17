c4022ba2f92541620dd94712a2b625f9c38552cc
David Rex <davidadekunle84@yahoo.com>
2024-08-17 09:16:50 +0100

    codes added to the file

---
+++ add.rb
@@ -0,0 +1,13 @@
+# Simple Arithmetic in Ruby
+a = 10
+b = 5
+
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+
+puts "Sum: #{sum}"
+puts "Difference: #{difference}"
+puts "Product: #{product}"
+puts "Quotient: #{quotient}"
---
+++ calc.py
@@ -0,0 +1,13 @@
+# Simple Arithmetic in Python
+a = 10
+b = 5
+
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
